11e2ae39fa6664f59a446c4c1258949e2fdf567f
Konstantin <kostiantyn.nechvolod@gmail.com>
2020-02-03 21:51:34 +0300

    Added fab button to home screen, added ttn recycler view and home ttn adapter

---
+++ app/src/main/java/com/nechvolod/konstantin/kernelapp/ui/adapter/HomeTtnAdapter.kt
@@ -0,0 +1,46 @@
+package com.nechvolod.konstantin.kernelapp.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.nechvolod.konstantin.kernelapp.base.adapter.BaseAdapter
+import com.nechvolod.konstantin.kernelapp.databinding.ItemTtnBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class HomeTtnAdapter: BaseAdapter<String, HomeTtnAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(
+            ItemTtnBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val selectEntity = getItem(position)
+        holder.bind(selectEntity)
+        holder.itemView.run {
+            setOnClickListener {
+                if (position != RecyclerView.NO_POSITION) {
+                    setOnClickListener?.invoke(selectEntity)
+                }
+            }
+        }
+    }
+    class ViewHolder(private val binding: ItemTtnBinding):
+            RecyclerView.ViewHolder(binding.root){
+        fun bind(item: String) {
+
+        }
+
+        private fun parceDate(oldDate: String):String{
+            var spf = SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.US)
+            val newDate = spf.parse(oldDate)
+            spf = SimpleDateFormat("dd.MM.yyyy", Locale.US)
+            return spf.format(newDate)
+        }
+    }
+}
--- app/src/main/java/com/nechvolod/konstantin/kernelapp/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/nechvolod/konstantin/kernelapp/ui/fragment/home/HomeFragment.kt
@@ -1,18 +1,63 @@
 package com.nechvolod.konstantin.kernelapp.ui.fragment.home
 
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.nechvolod.konstantin.kernelapp.BR
 import com.nechvolod.konstantin.kernelapp.R
 import com.nechvolod.konstantin.kernelapp.base.BaseFragment
+import com.nechvolod.konstantin.kernelapp.base.models.ToastModel
 import com.nechvolod.konstantin.kernelapp.databinding.FragmentHomeBinding
+import com.nechvolod.konstantin.kernelapp.ui.adapter.HomeTtnAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
+
+
+
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeVM>(HomeVM::class) {
     override fun getBindingViewModelId(): Int = BR.vm
 
     override fun getLayoutId(): Int = R.layout.fragment_home
 
+    private val homeAdapter: HomeTtnAdapter by lazy { HomeTtnAdapter() }
 
     override fun initFragmentViews(savedInstanceState: Bundle?) {
         super.initFragmentViews(savedInstanceState)
+        initRecycler()
+        setListeners()
+    }
+
+    private fun setListeners(){
+        fabAddTtn.setOnClickListener {
+            showToast(ToastModel(message = "Fab pressed!"))
+        }
+        rvTtn.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dy > 0 || dy < 0 && fabAddTtn.isShown)
+                    fabAddTtn.hide()
+            }
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE)
+                    fabAddTtn.show()
+                super.onScrollStateChanged(recyclerView, newState)
+            }
+        })
+    }
+
+    private fun initRecycler() {
+        val linearLayoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+        rvTtn.layoutManager = linearLayoutManager
+        rvTtn.adapter = homeAdapter
+        homeAdapter.replace(listOf("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""))
+
+        val divider = DividerItemDecoration(rvTtn.context, linearLayoutManager.orientation)
+        divider.setDrawable(requireContext().getDrawable(R.drawable.rv_divider)!!)
+        rvTtn.addItemDecoration(divider)
+        homeAdapter.setOnClickListener = {
+            showToast(ToastModel(message = it))
+        }
     }
 }
